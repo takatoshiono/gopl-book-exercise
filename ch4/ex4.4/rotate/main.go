1551c67d2f288ebfacaf28c354c0d18348aa88ab
Takatoshi Ono <takatoshi.ono@gmail.com>
2018-08-20 14:00:36 +0900

    Add rotate2 as cyclic replacements version
    ref: https://leetcode.com/problems/rotate-array/solution/

--- ch4/ex4.4/rotate/main.go
+++ ch4/ex4.4/rotate/main.go
@@ -1,15 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	ints := []int{1, 2, 3, 4, 5}
 	ints = rotate(ints, 2)
 	fmt.Println(ints) // expect [3 4 5 1 2]
+	rotate2(ints, 2)
+	fmt.Println(ints) // expect [5 1 2 3 4]
 }
 
 // Rotate s left by n positions.
 func rotate(s []int, n int) []int {
 	s = append(s, s[0:n]...)
 	return s[n:]
+}
+
+func rotate2(s []int, n int) {
+	n = n % len(s)
+	for start, count := 0, 0; count < len(s); start++ {
+		prev := s[start]
+		current := start
+		for {
+			next := (current - n) % len(s)
+			if next < 0 {
+				next = len(s) + next
+			}
+			temp := s[next]
+			s[next] = prev
+			prev = temp
+			current = next
+			count++
+			if current == start {
+				break
+			}
+		}
+	}
 }
